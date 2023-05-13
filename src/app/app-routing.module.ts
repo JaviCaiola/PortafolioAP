6eb1f9d10e646fdd9fd3262fb3aec3883592b1dc
Javier <javiteen1250@gmail.com>
2023-05-13 18:15:24 -0300

    actualizando bootstrapp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciosesionComponent } from '../app/components/iniciosesion/iniciosesion.component';
+import { PortfolioRouteComponent } from '../app/components/portfolio-route/portfolio-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'portfolio', component:PortfolioRouteComponent},
+  //{path:'inicio', component:IniciosesionComponent},
+  //{path:'', redirectTo:'portfolio', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
