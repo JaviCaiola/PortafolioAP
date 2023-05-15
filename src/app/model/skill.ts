d95a39527ba189153b35369d29ba67ec0d41feb9
Javier <javiteen1250@gmail.com>
2023-05-15 03:42:10 -0300

    creando model

---
+++ src/app/model/skill.ts
@@ -0,0 +1,10 @@
+export class Skill {
+    id: any;
+    nombre: String;
+    url: String;
+
+    constructor(nombre:String, url:String){
+        this.nombre = nombre;
+        this.url = url;
+    }
+}
