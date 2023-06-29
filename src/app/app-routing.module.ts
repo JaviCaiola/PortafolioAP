90a87e25ccbbc39f330234ff8179e1a8d5bce487
Javier <javiteen1250@gmail.com>
2023-06-29 19:18:04 -0300

    comenzando las validaciones del componente iniciar session

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioRouteComponent } from './components/portfolio-route/portfolio-route.component';
+import { FormSessionComponent } from './components/form-session/form-session.component';
 
 const routes: Routes = [
-  {path:'portfolio'component:}
+  {path:'portfolio',component:PortfolioRouteComponent},
+  {path:'iniciarsesion',component:FormSessionComponent},
+  {path:'',redirectTo:'iniciarsesion',pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/components/form-session/form-session.component.ts
+++ src/app/components/form-session/form-session.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-session',
   templateUrl: './form-session.component.html',
   styleUrls: ['./form-session.component.css']
 })
-export class FormSessionComponent {
+export class FormSessionComponent{
+  constructor(private formBuilder:FormBuilder){
 
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  
 }
