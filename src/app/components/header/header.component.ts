cc4d04d648586d36d74793405e530a5d8199accd
Javier <javiteen1250@gmail.com>
2023-06-29 18:57:40 -0300

    añadiendo logica de inicio de session al header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { FormSessionComponent } from '../form-session/form-session.component';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,11 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  
+  constructor(private _dialog: MatDialog){
+  }
+
+  iniciarSession(){
+    this._dialog.open(FormSessionComponent);
+   }
 
 }
