84636972fe0caf54f754c796578200a741eda204
Javier <javiteen1250@gmail.com>
2023-06-30 17:34:48 -0300

    completando primera fase, previo a los test

--- src/app/components/agregarskill/agregarskill.component.ts
+++ src/app/components/agregarskill/agregarskill.component.ts
@@ -32,9 +32,6 @@ export class AgregarskillComponent {
       })
     }
   }
-
-
-
   close(){
     this.dialogref.close();
   }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog'
-import { FormeditComponent } from '../formedit/formedit.component';
 import { EditskillComponent } from '../editskill/editskill.component';
+import { AgregarskillComponent } from '../agregarskill/agregarskill.component';
+import { Skill } from 'src/app/model/ObjSkill';
+import { AuthtenticationService } from 'src/app/service/authtentication.service';
 
 @Component({
   selector: 'app-skills',
@@ -9,16 +11,30 @@ import { EditskillComponent } from '../editskill/editskill.component';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
-  
+  skill : Array<Skill>;
 
-  constructor(private _dialog: MatDialog){
-    
+  constructor(private _dialog: MatDialog, private skService:AuthtenticationService){
+    this.skill = new Array<Skill>();
   }
-  agregarskill() {
-    this._dialog.open(FormeditComponent);
+
+  //INTERFACES QUE SE DESPLIEGAN
+  ventanaadd() {
+    this._dialog.open(AgregarskillComponent);
   }
 
   editarComponente( ){
     this._dialog.open(EditskillComponent);
+  }
+
+  //FUNCIONES DE LOS BOTONES Y OBTENCION DE DATOS
+  getSkill(){
+    this.skService.getAll().subscribe(res =>{
+      this.skill = res;
+    })
+  }
+  deleteSkill(id : number){
+    this.skService.delete(id).subscribe(res =>{
+      this.getSkill();
+    })
   }
 }
