80d9ee4fc27e746a916a76c58ddf3ea8d2209836
Javier <javiteen1250@gmail.com>
2023-06-30 16:39:23 -0300

    terminando la funcion crear

--- src/app/components/formedit/formedit.component.ts
+++ src/app/components/formedit/formedit.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Skill } from 'src/app/model/ObjSkill';
 import { AuthtenticationService } from 'src/app/service/authtentication.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formedit',
@@ -10,19 +12,29 @@ import { AuthtenticationService } from 'src/app/service/authtentication.service'
 })
 export class FormeditComponent {
 
+  skillo :Array<Skill>;
+  form : FormGroup;
+
   constructor (
-    private apiservice: AuthtenticationService,
-    public dialogref : MatDialogRef<FormeditComponent>
-  ){
-  }
-/*
-  agregarSkill(){
-    skill = Skill;
-    this.apiservice.create(skill).subscribe((data:any)=>{
-      this.dialogref.close();
+    public dialogref : MatDialogRef<FormeditComponent>, fb : FormBuilder, private skService : AuthtenticationService){
+    this.skillo = new Array<Skill>();
+    this.form = fb.group({
+      nombre : new FormControl('',[Validators.required]),
+      url : new FormControl('', [Validators.required])
     })
   }
-*/
+
+  crearSkill(){
+    if(this.form.valid){
+      let skill = new Skill();
+      skill.nombre = this.form.get('nombre')?.value;
+      skill.url = this.form.get('url')?.value;
+      this.skService.create(skill).subscribe(res => {
+        this.form.reset()
+      })
+    }
+  }
+
   close(){
     this.dialogref.close();
   }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog'
 import { FormeditComponent } from '../formedit/formedit.component';
 import { EditskillComponent } from '../editskill/editskill.component';
-import { Skill } from 'src/app/model/ObjSkill';
 
 @Component({
   selector: 'app-skills',
@@ -10,12 +9,11 @@ import { Skill } from 'src/app/model/ObjSkill';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
-  skillo = Array<Skill>;
+  
 
   constructor(private _dialog: MatDialog){
-    //this.skillo = new Array<Skill>();
+    
   }
-
   agregarskill() {
     this._dialog.open(FormeditComponent);
   }
