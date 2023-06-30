05259f8e6903053fc68948b57a01dd4be395d452
Javier <javiteen1250@gmail.com>
2023-06-30 17:08:08 -0300

    solucionando nevamentete el app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,7 +47,9 @@ import { AgregarskillComponent } from './components/agregarskill/agregarskill.co
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/agregarskill/agregarskill.component.ts
+++ src/app/components/agregarskill/agregarskill.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Skill } from 'src/app/model/ObjSkill';
+import { AuthtenticationService } from 'src/app/service/authtentication.service';
 
 @Component({
   selector: 'app-agregarskill',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AgregarskillComponent {
 
+  skill : Array<Skill>;
+  form : FormGroup;
+
+  constructor(fb : FormBuilder, private skService:AuthtenticationService){
+    this.skill = new Array<Skill>();
+    this.form =  fb.group({
+      nombre : new FormControl('',[Validators.required]),
+      url : new FormControl('', [Validators.required])
+    })
+  }
+
+  crearSkill(){
+    if(this.form.valid){
+      let skill = new Skill();
+      skill.nombre = this.form.get('nombre')?.value;
+      skill.url = this.form.get('url')?.value;
+      this.skService.create(skill).subscribe(res => {
+        this.form.reset()
+      })
+    }
+  }
+
+  
+  /*
+  close(){
+    this.dialogref.close();
+  }
+  */ 
 }
